bf1a278bff246d38f2032ab998c5265f04d7af9b
Harro Cyranka <harrofacyranka@gmail.com>
2018-07-31 16:20:04 -0400

    Tidy Week 18 - Submission 1
    Tidy Week 18 - Submission 1

---
+++ tidy_tuesday_week_18_sub_1.R
@@ -0,0 +1,69 @@
+remove(list = ls())
+options(stringsAsFactors = FALSE)
+options(scipen = 999)
+
+setwd("Working Directory")
+library(tidyverse);library(readxl);library(lubridate)
+
+
+x <- read_excel("week18_dallas_animals.xlsx", sheet =  "simple")
+outcomes_total <- x %>% count(outcome_type, sort = TRUE) %>% mutate(outcome_type = str_to_title(outcome_type)) %>%
+  mutate(proportion_of_total = n/sum(n)*100)
+
+outcomes_by_month <- x %>% mutate(floor_date = floor_date(intake_date, "month")) %>% group_by(outcome_type,floor_date) %>%
+  tally() %>% arrange(outcome_type, floor_date) %>% ungroup() %>%
+  mutate(outcome_type = str_to_title(outcome_type)) %>% filter(!outcome_type %in% c("Other", "Missing"))
+
+
+outcomes_by_month %>% ggplot(aes(x = floor_date, y = n, fill = outcome_type)) + geom_col(show.legend = FALSE) +
+  geom_smooth(se = FALSE, show.legend = FALSE) + 
+  facet_wrap(~outcome_type, scales = "free") + theme_bw() + 
+  labs(x = "Date", y= "Total Cases", title = "@Dallas Shelter Animal Data: Evolution of Outcomes Throughout FY 2017")
+
+outcomes_total %>% mutate(proportion_of_total = round(proportion_of_total,1)) %>%
+  ggplot(aes(x = reorder(outcome_type,n), y = n, label = n)) + geom_col(fill = "steelblue")+ theme_bw() + coord_flip() + 
+  geom_label() + labs(x = "Outcome Type", y = "Total", title = "@Dallas Shelter Animal Data: Total Outcome Types in FY 2017")
+
+##
+outcome_intake <- x %>% group_by(intake_type, outcome_type) %>% tally() %>%
+  ungroup() %>%
+  mutate(intake_type = str_to_title(intake_type),
+         outcome_type =str_to_title(outcome_type)) %>%
+  group_by(intake_type) %>% mutate(freq = n/sum(n),
+                                   total = sum(n)) %>%
+  select(intake_type, outcome_type, n, total, freq)
+
+##
+stray_surrender <- outcome_intake %>% filter(intake_type %in% c("Stray", "Owner Surrender"))
+
+library(scales)
+stray_surrender %>% select(-n,-total) %>%spread(intake_type, freq,fill = 0) %>%ggplot(aes(`Owner Surrender`, Stray)) +
+  geom_jitter(size = 2.5, width = 0, height = 0) +
+  geom_text(aes(label = outcome_type), check_overlap = TRUE, vjust = 1.5) +
+  scale_x_log10(labels = percent_format()) +
+  scale_y_log10(labels = percent_format()) +
+  geom_abline(color = "red", lty= 2) + theme_bw() +
+  labs(title = "Comparison of Outcomes: Stray Events x Owner Surrender (Two most frequent intake categories)")
+
+
+stray_surrender %>% select(-n,-total) %>%spread(intake_type, freq,fill = 0) %>%
+  mutate(logratio = log(`Stray`/`Owner Surrender`)) %>%
+  arrange(desc(logratio)) %>% mutate(outcome_type = reorder(outcome_type, logratio)) %>%
+  ggplot(aes(outcome_type, logratio, fill = logratio >0)) + geom_col(show.legend = FALSE) + coord_flip() + theme_bw() + 
+  labs(y = "Log Odds Ratio (Stray/Owner Surrender)", x= "Outcome Type",
+       title = "Outcomes Log Odds Ratio: Stray Events x Owner Surrender (Two most frequent intake categories)")
+  
+  
+##
+dev.off()
+
+##Look at Euthanized
+euthanized <- x %>% mutate(euthanized = ifelse(outcome_type == "EUTHANIZED",1,0)) %>%
+  mutate(intake_type = str_to_title(intake_type)) %>% filter(intake_type %in% c("Confiscated", "Owner Surrender", "Stray"))
+levels <- c( "Confiscated", "Owner Surrender", "Stray")
+euthanized <- euthanized %>% mutate(intake_type = factor(intake_type, levels = levels))
+
+fit.1 <- glm(euthanized ~ intake_type, family = binomial(link = "logit"), data = euthanized)
+arm::display(fit.1, detail = TRUE, digits = 4)
+
+arm::invlogit(-1.3658  + 0.1857)
