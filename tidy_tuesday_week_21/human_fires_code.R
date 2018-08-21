666338008e2772608a6672ca6cd537b3e1ba6ddf
Harro Cyranka <harrofacyranka@gmail.com>
2018-08-21 11:52:33 -0400

    First graphs

--- tidy_tuesday_week_21/human_fires_code.R
+++ tidy_tuesday_week_21/human_fires_code.R
@@ -15,13 +15,14 @@ causes_by_year <- z %>% group_by(year,fire_cause) %>%
   group_by(year) %>%
   mutate(percent = totals/sum(totals))
 
-causes_by_year %>% filter(fire_cause == "Human") %>%
+l <- (causes_by_year %>%
   ggplot(aes(x = year, y = percent*100)) + geom_line(size = 1.1, alpha = 0.5) + 
-  geom_smooth(se = FALSE, color = "red", size = 1.1) + theme_bw() + 
+  geom_smooth(se = FALSE, size = 1.1) + theme_bw() + 
   labs(x = "Year", y ="% of total fires",
-       title = "% of total fires caused by humans by year",
+       title = "% of total fires by year",
        subtitle = "Marked increase in the proportion of human-related fires since the 1950s",
-       caption = "Tidy Tuesday Week 21: California Fires")
+       caption = "Tidy Tuesday Week 21: California Fires") + facet_wrap(~fire_cause, nrow = 3) + 
+  theme_minimal())
 
 
 ##Size by Decades
@@ -30,14 +31,14 @@ decades <- z %>% mutate(decade = cut_width(year,10,boundary = 1950)) %>%
   arrange(year) %>% mutate(decade = factor(decade,labels = dec_labels))
   
 decades %>%
-  group_by(year, decade) %>%
+  group_by(year, decade, fire_cause) %>%
   summarise(mean_log_gis_acres = mean(log(gis_acres,base = 2),na.rm = TRUE)) %>%
   ggplot(aes(x = year, y = mean_log_gis_acres, color = decade)) +
   geom_line(size = 1.4) + theme_bw() + geom_smooth(size = 0.3,color = "black", se = FALSE) + 
   labs(x = "Year", color = "Decade", y = "Mean log acres of fire",
-       title = "Mean log acres of fire of human-related incidents",
-       subtitle = "There has been a visible decrease in the acreage of fires caused by humans \nSmooth line in black",
-       caption = "Tidy Tuesday Week 21: California Fires")
+       title = "Mean log acres of fire of fire-related incidents",
+       subtitle = "There has been a visible decrease in the acreage of fires \nSmooth line in black",
+       caption = "Tidy Tuesday Week 21: California Fires\nY-axis is Base 2 Log") + facet_wrap(~fire_cause, nrow = 3)
 
 
 x %>% mutate(month = month(ymd(alarm_date),label = TRUE)) %>% count(month, sort = TRUE)
