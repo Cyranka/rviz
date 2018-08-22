c649fc65b70a02a35aea1fba8374ad82e841b81b
Harro Cyranka <harrofacyranka@gmail.com>
2018-08-22 16:41:58 -0400

    begin modeling

--- tidy_tuesday_week_21/temperature_total_files.R
+++ tidy_tuesday_week_21/temperature_total_files.R
@@ -20,3 +20,23 @@ k %>%
   ggplot(aes(x = avg_temperature, y = total)) + geom_point() + theme_bw()
 
 
+##Use Only Natural
+n_1 <- z %>% group_by(year, month) %>%
+  filter(fire_cause %in% c("Natural")) %>%
+  summarise(total = n(),
+            avg_temperature = mean(mean_temperature,na.rm = TRUE))  %>%
+  filter(!is.na(month))
+
+n_1 %>%
+  ggplot(aes(x = avg_temperature, y = total)) + geom_point() + theme_bw()
+
+##No Outliers and fit model
+n_2 <- filter(n_1, total <180)
+
+n_2 %>%
+  ggplot(aes(x = avg_temperature, y = total)) + geom_point() + theme_bw()
+
+fit_1 <- glm(total~avg_temperature, data = n_2, family = "poisson")
+
+arm::display(fit_1)
+summary(fit_1)
