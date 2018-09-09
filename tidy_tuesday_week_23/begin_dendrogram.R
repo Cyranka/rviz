ff87e3e40e377f9d6ca1f1a5d300a0467f7654c5
Harro Cyranka <harrofacyranka@gmail.com>
2018-09-09 00:08:20 -0400

    Changes in dendogram

--- tidy_tuesday_week_23/begin_dendrogram.R
+++ tidy_tuesday_week_23/begin_dendrogram.R
@@ -23,12 +23,25 @@ y <- suppressWarnings(x %>% select_if(is_numeric) %>%
 set.seed(100)
 d <- y %>% select(calories:protein) %>%
     mutate_all(funs(scale(.,center = FALSE,scale = TRUE))) %>%
-    sample_frac(0.1) %>%
+    sample_frac(0.2) %>%
     dist()
 
 ##Apply cluster solution
 cc <- hclust(d, method = "average")
 plot(cc, cex = 0.5)
 
-cutree(cc, k=6)
-table(cutree(cc, k=6))
+cutree(cc, k=10)
+table(cutree(cc, k=10))
+
+##Get principal components to plot
+set.seed(100)
+pcFit <- prcomp(y %>% select(calories:protein) %>%
+    mutate_all(funs(scale(.,center = FALSE,scale = TRUE))) %>%
+    sample_frac(0.2))
+
+pcFit$x[,1:2] %>% as_tibble() %>%
+    mutate(cluster = cutree(cc, k = 10)) %>%
+    filter(cluster <3) %>%
+    ggplot(aes(x= PC1, y = PC2, color = as.character(cluster))) +
+    geom_point(show.legend = FALSE) + theme_minimal()
+    
