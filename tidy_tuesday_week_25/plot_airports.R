d98be55f2dedc744fdd41aabbbce54388f33652f
Harro Cyranka <harrofacyranka@gmail.com>
2018-09-18 16:59:22 -0400

    Plot Airports

---
+++ tidy_tuesday_week_25/plot_airports.R
@@ -0,0 +1,44 @@
+remove(list = ls())
+options(stringsAsFactors = FALSE)
+options(scipen = 999)
+
+setwd("/Users/harrocyranka/Desktop/rviz/tidy_tuesday_week_25/")
+library(tidyverse)
+
+x <- read_csv("us_airports.csv")
+y <- read_csv("states_abbreviations.csv")
+z <- read_csv("airports_locations.csv")
+
+##
+matched_airports <- x %>% filter(year == 2016) %>%
+  inner_join(y, by = c("state"= "abbreviation")) %>%
+  select(-state) %>%
+  rename(state = state.y) %>%
+  select(state, loc_id, passengers, airport_classification) %>%
+  inner_join(z, by = c("loc_id" = "iata")) %>%
+  filter(country == "United States" & !state %in% c("Alaska", "Hawaii"))
+
+urbnmapr::states %>% filter(!state_abbv %in% c("AK","HI")) %>%
+  ggplot(aes(long, lat, group = group)) + 
+  geom_polygon() + 
+  geom_polygon(size = .45,color = "gray35", fill = "black") +
+  theme_minimal() +
+  geom_point(aes(long, lat,size = passengers,
+                 color = airport_classification), inherit.aes = FALSE, data = matched_airports %>%
+               filter(long > -140), alpha = 0.65) + 
+  coord_map(projection = "albers", lat0 = 39, lat1 = 45) + 
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.text.x = element_blank(),
+        axis.title.x = element_blank(),
+        axis.text.y = element_blank(),
+        axis.title.y = element_blank(),
+        legend.position = "bottom") + 
+  labs(color = "Airport classification", size = "Total passengers") + 
+  guides(color = guide_legend(title.position = "top", 
+                             title.hjust = 0.5, barwidth = 20,
+                             barheight = 0.5,keywidth = 4,keyheight = 1,nrow = 3, ncol = 1),
+         size = guide_legend(title.position = "top",
+                             title.hjust = 0.5,
+                             ncol = 1)) + 
+  scale_color_manual(values = c("red", "chocolate1", "yellow"))
