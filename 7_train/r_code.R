0e84755c92ea65a469009fd51840df182f7a0655
Harro Cyranka <harrofacyranka@gmail.com>
2018-11-19 14:45:12 -0500

    change in code

--- 7_train/r_code.R
+++ 7_train/r_code.R
@@ -2,8 +2,11 @@ remove(list = ls())
 options(stringsAsFactors = FALSE)
 options(scipen = 999)
 
-setwd("/Users/harrocyranka/Desktop/7_train/")
+setwd("/Users/harrocyranka/Desktop/rviz/7_train/")
 library(tidyverse);library(tidytext)
+library(igraph)
+library(ggraph)
+library(widyr)
 
 x <- readRDS("train_7_tweets.RDS") %>%
   mutate(text = rtweet::plain_tweets(text))
@@ -16,17 +19,11 @@ tidy_delays <- delays %>% select(status_id, text) %>%
   unnest_tokens(word,text,token = "tweets") %>% anti_join(stop_words) %>%
   filter(nchar(word) > 2)
 
-tdm_delays <- tidy_delays %>% group_by(status_id, word) %>% tally() %>%
-  cast_tdm(word, status_id, n)
-
-##Associations: Times square and Grand central
-t_square <- tm::findAssocs(tdm_delays, "times",0.05)$times %>% broom::tidy()
-
-g_central <- tm::findAssocs(tdm_delays, "grand",0.05)$grand %>% broom::tidy() %>% filter(x <0.5)
 
 ##
-tdm2 <- tm::removeSparseTerms(tdm_delays, sparse = 0.98)
+word_correlations <- tidy_delays %>% group_by(word) %>% filter(n() >=10) %>% anti_join(data_frame(word = c("sthudson","stelmhurst",
+                                                                                                           "stbound","metswillets","flushingmain",
+                                                                                                           "blvdjackson","blvdjackson","stbroadway"))) %>%
+  pairwise_cor(word, status_id, sort = TRUE, upper = FALSE) %>% filter(!is.infinite(correlation))
 
 
-hc <- hclust(dist(tdm2), method = "complete")
-plot(hc, yaxt = "n", main = "7 train delays",hang = -1, cex = 0.8)
