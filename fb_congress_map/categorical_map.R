03341d797cfe1536dcf60838b4f3b247a2961c7a
Harro Cyranka <harrofacyranka@gmail.com>
2019-03-11 08:23:35 -0400

    Push changes

--- fb_congress_map/categorical_map.R
+++ fb_congress_map/categorical_map.R
@@ -49,11 +49,12 @@ map_data <- USAboundaries::us_congressional(resolution = "low") %>%
 
 continental <- map_data %>% filter(!state_abbr %in% c("HI", "AK")) %>%
     mutate(largest_group = factor(largest_group,
-                                  levels = c("Left", "Moderate", "Right"))) %>%
+                                  levels = c("Left", "Moderate", "Right"),
+                                  labels = c("Progressive", "Moderate", "Conservative"))) %>%
     ggplot(aes(fill = largest_group)) +
-    labs(title = "Largest political group on Facebook (March 2017)",
+    labs(title = "Largest political group on Facebook by congressional district (March 2017)",
          subtitle = "Data collected from the Facebook Marketing API",
-         caption = "Right represents Conservatives and Very Conservatives in each district\nLeft represents Liberals and Very Liberals in each district"
+         caption = "Conservative represents Conservatives and Very Conservatives in each district\nProgressive represents Liberals and Very Liberals in each district"
          ) + 
     geom_sf(color = "gray40", alpha = 0.8) + 
     coord_sf(crs = 6350, datum = NA)  + 
@@ -77,3 +78,59 @@ continental <- map_data %>% filter(!state_abbr %in% c("HI", "AK")) %>%
                               keyheight = 0.5))
     
 
+continental
+
+# Map with score ----------------------------------------------------------
+with_score <- x %>%
+    mutate(group = case_when(
+        Politics %in% c("Very Liberal", "Liberal") ~ "Left",
+        Politics %in% c("Very Conservative", "Conservative") ~ "Right",
+        TRUE ~ "Moderate"
+    )) %>%
+    group_by(district,group) %>%
+    summarise(
+        total = sum(Count)
+    ) %>%
+    spread(group, total) %>%
+    mutate(score = log(Left/Right,2)) %>%
+    select(district, score)
+
+z2 <- y %>% inner_join(with_score)
+
+grad_map <- USAboundaries::us_congressional(resolution = "low") %>%
+    inner_join(z2 %>% select(state_abbr, cd115fp, score))
+
+
+grad_map %>% filter(!state_abbr %in% c("HI", "AK"))  %>%
+    ggplot(aes(fill = score)) +
+    labs(title = "    How liberal or conservative was a congressional district on Facebook? (March 2017)",
+         subtitle = "     Data collected from the Facebook Marketing API",
+         caption = "Measure calculated as: log((total progressives)/(total conservatives))\nModerates excluded"
+    ) + 
+    geom_sf(color = "gray40") + 
+    coord_sf(crs = 6350, datum = NA)  + 
+    theme_void() + 
+    scale_fill_gradient2(
+        low = "firebrick4",
+        mid = "gainsboro",
+        high = "dodgerblue3",
+        midpoint = 0,
+        breaks = c(-2.76,0,5.05),
+        labels = c("Most conservative district:\nKY-05",
+                   "",
+                   "Most liberal district:\nNY-15")
+    ) + 
+    theme(
+        text = element_text(family = "Roboto"),
+        panel.grid = element_blank(),
+        legend.position = "bottom",
+        plot.title = element_text(size = 18, face = "bold"),
+        plot.subtitle = element_text(size = 15),
+        legend.title = element_text(face = "bold", size = 12),
+        legend.text = element_text(size = 10)
+    ) + 
+    guides(fill = guide_colorbar(
+        title = "Log 2 ratio of progressives to conservatives",
+        title.position = "top",
+        title.hjust = 0.5,barwidth = 20,barheight = 0.5
+    ))
